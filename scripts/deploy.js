dd5449123a8de250f48e0dd716741b8dd6d33b55
Amaury Carrade <amaury@carrade.eu>
2021-05-03 18:09:00 +0200

    Deploy to GH-Pages

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+const execa = require('execa');
+
+(async () => {
+  try {
+    await execa('git', ['checkout', '--orphan', 'gh-pages'])
+    console.log('Building...')
+    await execa('npm', ['run', 'build'])
+    await execa('git', ['--work-tree', 'dist', 'add', '--all'])
+    await execa('git', ['--work-tree', 'dist', 'commit', '-m', 'GitHub Pages build'])
+    console.log('Pushing to gh-pages...')
+    await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force'])
+    await execa('rm', ['-r', 'dist'])
+    await execa('git', ['checkout', '-f', 'master'])
+    await execa('git', ['branch', '-D', 'gh-pages'])
+    console.log('Successfully deployed')
+  } catch (e) {
+    console.log(e.message)
+    process.exit(1)
+  }
+})()
