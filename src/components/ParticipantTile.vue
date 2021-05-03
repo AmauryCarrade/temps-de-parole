8e038c42f4f90b8c07fb209f68a16c70718f75ee
Amaury Carrade <amaury@carrade.eu>
2021-05-03 16:17:59 +0200

    Base features

---
+++ src/components/ParticipantTile.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="participant-tile" :class="{ 'is-talking': participant.talking }" @click="toggle_talk(uuid)">
+    <h1>{{ participant.name }}</h1>
+    <p class="timer" :class="{ 'has-hours': has_hours }">{{ duration }}</p>
+    <p class="percentage" v-if="percentage != null">{{ percentage }}%</p>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  props: {
+    uuid: String
+  },
+  computed: {
+    ...mapState(['participants']),
+
+    participant () {
+      return this.participants[this.uuid]
+    },
+
+    duration () {
+      const dateElapsed = new Date(this.participant.elapsed * 1000)
+      const hours = dateElapsed.getUTCHours()
+      const minutes = dateElapsed.getUTCMinutes()
+      const seconds = dateElapsed.getUTCSeconds()
+
+      const formatted = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+
+      if (hours > 0) {
+        return `${hours.toString().padStart(2, '0')}:${formatted}`
+      } else {
+        return formatted
+      }
+    },
+
+    has_hours () {
+      return this.participant.elapsed >= 3600
+    },
+
+    percentage () {
+      const total = Object.keys(this.participants).map(uuid => this.participants[uuid].elapsed).reduce((sum, elapsed) => sum + elapsed, 0)
+      if (total === 0) return null
+      return Math.ceil((this.participant.elapsed / total) * 100)
+    }
+  },
+
+  methods: mapActions(['toggle_talk'])
+}
+</script>
+
+<style scoped lang="sass">
+.participant-tile
+  display: flex
+  flex-direction: column
+  justify-content: space-between
+
+  padding: .4rem 0
+
+  border-radius: 4px
+  background-color: hsl(276, 91%, 23%)
+  color: hsl(262, 90%, 96%)
+
+  text-align: center
+  cursor: pointer
+
+  transition: background-color .1s ease-in-out
+
+  &.is-talking
+    background-color: hsl(274, 87%, 31%)
+
+  h1, p
+    margin: 0
+
+  h1
+    font-weight: 400
+
+  .timer
+    font-family: 'Fira Code', monospace
+    font-weight: 300
+    letter-spacing: -.051em
+    font-size: 3.6rem
+
+    &.has-hours
+      font-size: 2.2rem
+
+  .percentage
+    font-size: 1.2rem
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,101 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import { v4 as uuidv4 } from 'uuid'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    participants: {}
   },
   mutations: {
+    add_participants (state, participants) {
+      state.participants = participants
+    },
+
+    add_participant (state, name) {
+      const uuid = uuidv4()
+
+      Vue.set(state.participants, uuid, {
+        uuid,
+        name,
+        elapsed: 0,
+        talking: false
+      })
+    },
+
+    remove_participant (state, uuid) {
+      Vue.set(state.participants, uuid, undefined)
+    },
+
+    reset (state) {
+      state.participants = {}
+    },
+
+    talk (state, { uuid, talking }) {
+      Vue.set(this.state.participants[uuid], 'talking', talking)
+    },
+
+    tick_one_second (date, uuid) {
+      Vue.set(this.state.participants[uuid], 'elapsed', this.state.participants[uuid].elapsed + 1)
+    }
   },
+
   actions: {
-  },
-  modules: {
+    save (context) {
+      localStorage.setItem('participants', JSON.stringify(context.state.participants))
+    },
+
+    reset (context) {
+      context.commit('reset')
+      localStorage.removeItem('participants')
+    },
+
+    add_participants (context, participants) {
+      context.commit('add_participants', participants)
+    },
+
+    add_participant (context, name) {
+      context.commit('add_participant', name || 'Sans nom')
+      context.dispatch('save')
+    },
+
+    remove_participant (context) {
+      context.commit('remove_participant')
+      context.dispatch('save')
+    },
+
+    toggle_talk (context, uuid) {
+      const wasTalking = this.state.participants[uuid].talking
+      context.dispatch('stop_talking')
+
+      if (!wasTalking) {
+        context.commit('talk', {
+          uuid,
+          talking: true
+        })
+      }
+      context.dispatch('save')
+    },
+
+    stop_talking (context) {
+      for (const uuid of Object.keys(this.state.participants)) {
+        context.commit('talk', {
+          uuid,
+          talking: false
+        })
+      }
+      context.dispatch('save')
+    },
+
+    tick_every_second (context) {
+      for (const uuid of Object.keys(this.state.participants)) {
+        if (this.state.participants[uuid].talking) {
+          context.commit('tick_one_second', uuid)
+        }
+      }
+      context.dispatch('save')
+    }
   }
 })
