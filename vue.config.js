031072a4eb779719f06074394c349e84ef50143b
Amaury Carrade <amaury@carrade.eu>
2021-05-03 18:18:54 +0200

    Settings for non-root base path + PWA

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  publicPath: '/temps-de-parole/',
+
+  pwa: {
+    name: 'Temps de parole',
+    themeColor: '#44056E',
+    msTileColor: '#44056E',
+    manifestOptions: {
+      background_color: '#44056E'
+    }
+  }
+}
